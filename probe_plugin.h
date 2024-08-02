5e7a3eeec1daab4e8b3169a60a2039dfa844c801
Sienci Labs <chris_thorogood@sienci.com>
2024-08-02 17:56:51 -0400

    Update probe_plugin.h

--- probe_plugin.h
+++ probe_plugin.h
@@ -5,9 +5,6 @@
   Part of grblHAL
   grblHAL is
   Copyright (c) 2022-2023 Terje Io
-  
-  Plugin code is
-  Copyright (c) 2023 Expatria Technologies
 
   Grbl is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -21,6 +18,32 @@
 
   You should have received a copy of the GNU General Public License
   along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
+
+  
+  Plugin code is
+  Copyright (c) 2023 Expatria Technologies
+  Copyright (c) Sienci Labs Inc.
+  
+   This file is part of the SuperLongBoard family of products.
+  
+   This source describes Open Hardware and is licensed under the "CERN-OHL-S v2"
+
+   You may redistribute and modify this source and make products using
+   it under the terms of the CERN-OHL-S v2 (https://ohwr.org/cern_ohl_s_v2.t). 
+   This source is distributed WITHOUT ANY EXPRESS OR IMPLIED WARRANTY,
+   INCLUDING OF MERCHANTABILITY, SATISFACTORY QUALITY AND FITNESS FOR A 
+   PARTICULAR PURPOSE. Please see the CERN-OHL-S v2 for applicable conditions.
+   
+   As per CERN-OHL-S v2 section 4, should You produce hardware based on this 
+   source, You must maintain the Source Location clearly visible on the external
+   case of the CNC Controller or other product you make using this source.
+  
+   You should have received a copy of the CERN-OHL-S v2 license with this source.
+   If not, see <https://ohwr.org/project/cernohl/wikis/Documents/CERN-OHL-version-2>.
+   
+   Contact for information regarding this program and its license
+   can be sent through gSender@sienci.com or mailed to the main office
+   of Sienci Labs Inc. in Waterloo, Ontario, Canada.
 
 */
 
